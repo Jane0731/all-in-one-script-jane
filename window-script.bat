8640edee33b3533aad81f48b0fcc72dd98d2c323
Jane0731 <jane991680420@outlook.com>
2022-03-25 14:36:01 +0800

    Add choco install chocolateygui

--- window-script.bat
+++ window-script.bat
@@ -8,6 +8,7 @@ choco install -y python
 choco install -y nvm
 choco install -y composer
 choco install -y virtualbox
+choco install -y chocolateygui
 choco install -y vagrant
 choco install -y mysql
 :: communication
